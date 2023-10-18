53c3fafd7e48a121dcf669151a6c8b79e5e0511c
sahhhall <138967723+sahhhall@users.noreply.github.com>
2023-10-18 19:50:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// selcting query
+let toggleLogo=document.querySelector('.toggle-logo')
+let menu=document.querySelector('.menu ')
+
+toggleLogo.addEventListener('click',()=>{
+   menu.classList.toggle('is-active')
+   
+
+})
